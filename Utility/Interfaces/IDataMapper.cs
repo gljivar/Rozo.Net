29eae1c7d3164b7d3fad7052178288967c6711f7
Miroslav V <gljivar@gmail.com>
2012-09-12 00:41:57 +0200

    Some testing for deletion and getting of objects

---
+++ Utility/Interfaces/IDataMapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utility.Interfaces
+{
+    public interface IDataMapper<T>
+    {
+        void Create(T item);
+        void Update(T item);
+        void Delete(T item);
+
+        void DeleteById(int id);
+    }
+}
